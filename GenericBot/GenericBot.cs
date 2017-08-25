49e94efc960648bab610d3ffa7be4447c622ddf0
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-08-24 18:50:49 -0700

    Moved Program to GenericBot

---
+++ GenericBot/GenericBot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+
+namespace GenericBot
+{
+    class GenericBot
+    {
+        protected static DiscordShardedClient DiscordClient;
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
