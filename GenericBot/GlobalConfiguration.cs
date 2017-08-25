94b8d68d3ab3debb5e98fa7e1f8a82ed25790676
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-08-24 17:31:55 -0700

    Blank Config Constructor + AddBlacklists

--- GenericBot/GlobalConfiguration.cs
+++ GenericBot/GlobalConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Discord.WebSocket;
 
 namespace GenericBot
 {
@@ -22,5 +24,100 @@ namespace GenericBot
         public List<ulong> BlacklistedOwnerIds { get; set; }
         //Total Blacklist of Death (no ownership OR commands)
         public List<ulong> BlacklistedUserIds { get; set; }
+
+        public GlobalConfiguration()
+        {
+            Token = "";
+            DefaultPrefix = ">";
+            DefaultExecuteEdits = true;
+            OwnerId = new ulong();
+            GlobalAdminIds = new List<ulong>();
+            
+            BlacklistedGuildIds = new List<ulong>();
+            BlacklistedOwnerIds = new List<ulong>();
+            BlacklistedUserIds = new List<ulong>();
+        }
+        
+        //Add To various blacklists
+        //return values
+        //0 SUCCESS, 1 FORBIDDEN, 2 ALREADY CONTAINED
+        //Add a guildId to the guild blacklist
+        public int AddGuildBlacklist(DiscordSocketClient socketClient, ulong guildId)
+        {
+            try
+            {
+                var guild = socketClient.GetGuild(guildId);
+                if (OwnerId.Equals(guild.OwnerId))
+                {
+                    Console.WriteLine("Cannot Blacklist a Guild owned by the Bot Owner");
+                    return 1;
+                }
+                if (GlobalAdminIds.Contains(guild.OwnerId))
+                {
+                    Console.WriteLine("Cannot Blacklist a Guild owned by a Global Admin");
+                    return 1;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            if (BlacklistedGuildIds.Contains(guildId))
+            {
+                Console.WriteLine("Requested Guild is already Blacklisted");
+                return 2;
+            }
+            
+            BlacklistedGuildIds.Add(guildId);
+            Console.WriteLine($"Added {guildId} to the Guild Blacklist");
+            return 0;
+        } 
+        //Add a userId to the guildOwner blacklist
+        public int AddOwnerBlacklist(ulong userId)
+        {
+            if (OwnerId.Equals(userId))
+            {
+                Console.WriteLine("Cannot Blacklist Bot Owner");
+                return 1;
+            }
+            if (GlobalAdminIds.Contains(userId))
+            {
+                Console.WriteLine("Cannot Blacklist Global Admin");
+                return 1;
+            }
+            if (BlacklistedOwnerIds.Contains(userId))
+            {
+                Console.WriteLine("Requested GuildOwner is already Blacklisted");
+                return 2;
+            }
+            
+            BlacklistedOwnerIds.Add(userId);
+            Console.WriteLine($"Added {userId} to the Guild Owner Blacklist");
+            return 0;
+        }
+        //Add a userId to the User blacklist
+        public int AddUserBlacklist(ulong userId)
+        {
+            if (OwnerId.Equals(userId))
+            {
+                Console.WriteLine("Cannot Blacklist Bot Owner");
+                return 1;
+            }
+            if (GlobalAdminIds.Contains(userId))
+            {
+                Console.WriteLine("Cannot Blacklist Global Admin");
+                return 1;
+            }
+            if (BlacklistedOwnerIds.Contains(userId))
+            {
+                Console.WriteLine("Requested User is already Blacklisted");
+                return 2;
+            }
+            
+            BlacklistedOwnerIds.Add(userId);
+            Console.WriteLine($"Added {userId} to the User Blacklist");
+            return 0;
+        }
     }
 }
