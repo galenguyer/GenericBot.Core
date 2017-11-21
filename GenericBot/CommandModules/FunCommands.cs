10279c6b780124a76ddc8e95d39e5943f10d8b2a
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-11-21 12:33:05 -0800

    markov command

--- GenericBot/CommandModules/FunCommands.cs
+++ GenericBot/CommandModules/FunCommands.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Discord;
+using Discord.Net.Queue;
 using GenericBot.Entities;
+using MarkVSharp;
 
 namespace GenericBot.CommandModules
 {
@@ -17,6 +21,27 @@ namespace GenericBot.CommandModules
             };
 
             FunCommands.Add(wat);
+
+            Command markov = new Command("markov");
+            markov.Description = "Create a markov chain from the last messages in the channel";
+            markov.Delete = true;
+            markov.Usage = "markov";
+            markov.ToExecute += async (client, msg, parameters) =>
+            {
+                var messages = msg.Channel.GetMessagesAsync().Flatten().Result.Reverse().Select(m =>m.Content).ToList();
+                messages.ToList().AddRange(messages.TakeLast(50));
+                messages.ToList().AddRange(messages.TakeLast(25));
+                messages.ToList().AddRange(messages.TakeLast(10));
+
+                int averageLength = messages.Sum(m => m.Split(' ').Length) / 185;
+                averageLength = averageLength > 10 ? averageLength : averageLength * 2;
+
+                var markovGenerator = new MarkovGenerator(messages.Aggregate((i, j) => i.TrimEnd('.') + ". " + j));
+
+                await msg.ReplyAsync(markovGenerator.GenerateSentence(averageLength));
+            };
+
+            FunCommands.Add(markov);
 
             return FunCommands;
         }
