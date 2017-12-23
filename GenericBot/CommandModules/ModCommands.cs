7322dfd2b44d769094de9ab245eeaf0f3996bfb7
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-12-22 19:41:40 -0800

    whois command

--- GenericBot/CommandModules/ModCommands.cs
+++ GenericBot/CommandModules/ModCommands.cs
@@ -8,6 +8,7 @@ using Discord.Net.Queue;
 using Discord.Rest;
 using Discord.WebSocket;
 using GenericBot.Entities;
+using LiteDB;
 
 namespace GenericBot.CommandModules
 {
@@ -63,6 +64,99 @@ namespace GenericBot.CommandModules
                     await msg.ReplyAsync("That's not a valid number");
                 }
             };
+
+            Command whois = new Command("whois");
+            whois.Description = "Get information about a user";
+            whois.Usage = "whois @user";
+            whois.RequiredPermission = Command.PermissionLevels.Moderator;
+            whois.ToExecute += async (client, msg, parameters) =>
+            {
+                await msg.GetGuild().DownloadUsersAsync();
+                if (!msg.GetMentionedUsers().Any())
+                {
+                    await msg.ReplyAsync("No user found");
+                    return;
+                }
+                SocketGuildUser user;
+                ulong uid = msg.GetMentionedUsers().FirstOrDefault().Id;
+
+                if (msg.GetGuild().Users.Any(u => u.Id == uid))
+                {
+                    user = msg.GetGuild().GetUser(uid);
+                }
+                else
+                {
+                    await msg.ReplyAsync("User not found");
+                    return;
+                }
+
+                string nickname = msg.GetGuild().Users.All(u => u.Id != uid) || string.IsNullOrEmpty(user.Nickname) ? "None" : user.Nickname;
+                string roles = "";
+                foreach (var role in user.Roles.Where(r => !r.Name.Equals("@everyone")).OrderByDescending(r => r.Position))
+                {
+                    roles += $"`{role.Name}`, ";
+                }
+                DBUser dbUser;
+                using (var db = new LiteDatabase(GenericBot.DBConnectionString))
+                {
+                    var col = db.GetCollection<DBGuild>("userDatabase");
+                    col.EnsureIndex(c => c.ID, true);
+                    DBGuild guildDb;
+                    if(col.Exists(g => g.ID.Equals(msg.GetGuild().Id)))
+                        guildDb = col.FindOne(g => g.ID.Equals(msg.GetGuild().Id));
+                    else guildDb = new DBGuild (msg.GetGuild().Id);
+                    if (guildDb.Users.Any(u => u.ID.Equals(user.Id))) // if already exists
+                    {
+                        dbUser = guildDb.Users.First(u => u.ID.Equals(user.Id));
+                    }
+                    else
+                    {
+                        dbUser = new DBUser(user);
+                        col.Upsert(guildDb);
+                    }
+                    db.Dispose();
+                }
+
+                string uns = "";
+                string nns = "";
+                foreach (var s in dbUser.Usernames.Distinct())
+                {
+                    uns += $"`{s}`, ";
+                }
+                uns = uns.Trim(',');
+                if (!dbUser.Nicknames.Empty())
+                {
+                    foreach (var s in dbUser.Nicknames.Distinct())
+                    {
+
+                    }
+                }
+
+                string info =  $"User Id:  `{user.Id}`\n";
+                info += $"Username: `{user.ToString()}`\n";
+                info += $"Past Usernames: `{dbUser.Usernames.Distinct().ToList().reJoin(", ")}`\n";
+                info += $"Nickname: `{nickname}`\n";
+                if(!dbUser.Nicknames.Empty())
+                    info += $"Past Nicknames: `{dbUser.Nicknames.Distinct().ToList().reJoin(", ")}`\n";
+                info += $"Created At: `{string.Format("{0:yyyy-MM-dd HH\\:mm\\:ss zzzz}", user.CreatedAt.LocalDateTime)}GMT` " +
+                        $"(about {(DateTime.UtcNow - user.CreatedAt).Days} days ago)\n";
+                if (user.JoinedAt.HasValue)
+                    info +=
+                        $"Joined At: `{string.Format("{0:yyyy-MM-dd HH\\:mm\\:ss zzzz}", user.JoinedAt.Value.LocalDateTime)}GMT`" +
+                        $"(about {(DateTime.UtcNow - user.JoinedAt.Value).Days} days ago)\n";
+                info += $"Roles: {roles.Trim(' ', ',')}\n";
+                if(dbUser.Warnings.Any())
+                    info += $"`{dbUser.Warnings.Count}` Warnings: {dbUser.Warnings.reJoin(" | ")}";
+
+
+                foreach (var str in info.SplitSafe(','))
+                {
+                    await msg.ReplyAsync(str.TrimStart(','));
+                }
+
+            };
+
+            ModCommands.Add(whois);
 
             Command archive = new Command("archive");
             archive.RequiredPermission = Command.PermissionLevels.Admin;
