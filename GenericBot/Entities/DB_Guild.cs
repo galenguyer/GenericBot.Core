8cc19960df00b199675fb262170a1ed782f7ad65
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-12-22 19:41:02 -0800

    DBGuild+DBUser

---
+++ GenericBot/Entities/DB_Guild.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GenericBot.Entities
+{
+    public class DBGuild
+    {
+        public ulong ID { get; set; }
+        public List<DBUser> Users { get; set; }
+
+        public DBGuild()
+        {
+
+        }
+        public DBGuild(ulong Id)
+        {
+            ID = Id;
+            Users = new List<DBUser>();
+        }
+    }
+}
---
+++ GenericBot/Entities/DB_User.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Discord.WebSocket;
+
+namespace GenericBot.Entities
+{
+    public class DBUser
+    {
+        public ulong ID { get; set; }
+        public List<string> Nicknames { get; set; }
+        public List<string> Usernames { get; set; }
+        public List<string> Warnings { get; set; }
+
+        public DBUser()
+        {
+
+        }
+        public DBUser(SocketGuildUser user)
+        {
+            ID = user.Id;
+            Nicknames = new List<string>();
+            Usernames = new List<string>{user.Username};
+            Warnings = new List<string>();
+
+            if(!string.IsNullOrEmpty(user.Nickname))
+                Nicknames.Add(user.Nickname);
+        }
+
+        public void AddUsername(string username)
+        {
+            if (!Usernames.Contains(username))
+            {
+                Usernames.Add(username);
+            }
+        }
+
+        public void AddNickname(SocketGuildUser user)
+        {
+            if (!string.IsNullOrEmpty(user.Nickname) && !Nicknames.Contains(user.Nickname))
+            {
+                Nicknames.Add(user.Nickname);
+            }
+        }
+
+        public void AddWarning(string warning)
+        {
+            Warnings.Add(warning);
+        }
+    }
+}
