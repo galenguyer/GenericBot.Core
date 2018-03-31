a807b0262100954dfae08ea258b1df7d8f11cc94
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-03-31 08:49:21 -0700

    added roll command

--- GenericBot/CommandModules/FunCommands.cs
+++ GenericBot/CommandModules/FunCommands.cs
@@ -43,6 +43,80 @@ namespace GenericBot.CommandModules
 
             FunCommands.Add(markov);
 
+            Command roll = new Command("roll");
+            roll.Aliases.Add("dice");
+            roll.Description = "Roll a specified number of dices. Defaults to 1d20 if no parameters";
+            roll.Usage = "roll [count]d[sides]";
+            roll.ToExecute += async (client, msg, parameters) =>
+            {
+                int count = 1;
+                int sides = 20;
+                if(!parameters.Empty())
+                {
+                    string param = parameters.reJoin("").ToLower();
+                    if (!param.Contains("d"))
+                    {
+                        if (!int.TryParse(param, out count))
+                        {
+                            await msg.ReplyAsync("Input improperly formatted");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        var list = param.Split('d');
+                        if (!(int.TryParse(list[0], out count) && int.TryParse(list[1], out sides)))
+                        {
+                            await msg.ReplyAsync("Input improperly formatted");
+                            return;
+                        }
+                    }
+                }
+
+                if (count > 100)
+                {
+                    await msg.ReplyAsync("I don't think you can hold that many!");
+                    return;
+                }
+                if (sides < 2)
+                {
+                    await msg.ReplyAsync("You can't have a 1-sided dice!");
+                    return;
+                }
+                if (sides > 100)
+                {
+                    await msg.ReplyAsync("That's an awefully large dice, I can't hold that");
+                    return;
+                }
+                RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+                List<int> results = new List<int>();
+                while(results.Count < count)
+                {
+                    byte[] bytes = new byte[4];
+                    crypto.GetNonZeroBytes(bytes);
+                    int rand = Math.Abs(BitConverter.ToInt32(bytes, 0)) % sides;
+                    results.Add(rand + 1);
+                }
+
+                string res = $"{msg.Author.Mention}, you rolled ";
+                results.Sort();
+                for (int i = 0; i < results.Count; i++)
+                {
+                    if (i == results.Count - 1 && results.Count > 1)
+                    {
+                        res += $"and `{results.ElementAt(i)}`";
+                    }
+                    else
+                    {
+                        res += $"`{results.ElementAt(i)}`, ";
+                    }
+                }
+
+                await msg.ReplyAsync(res.TrimEnd(',', ' '));
+            };
+
+            FunCommands.Add(roll);
+
             return FunCommands;
         }
     }
