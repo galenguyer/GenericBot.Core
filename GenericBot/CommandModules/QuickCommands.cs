45ee9d0f8569557a7f80736826f8e02ee5f5c25b
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-04-18 18:11:20 -0700

    quickCommand system

---
+++ GenericBot/CommandModules/QuickCommands.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Net.Queue;
+using Discord.WebSocket;
+using GenericBot.Entities;
+
+namespace GenericBot.CommandModules
+{
+    public class QuickCommands
+    {
+        protected class QuickCommand
+        {
+            private string _name;
+            private string _returnValue;
+            private string _description;
+            public QuickCommand(string name, string returnValue, string description = "")
+            {
+                this._name = name;
+                this._returnValue = returnValue;
+                this._description = string.IsNullOrEmpty(description) ? null : description;
+            }
+
+            public Command GetCommand()
+            {
+                Command cmd = new Command(_name);
+                cmd.SendTyping = false;
+                cmd.Description = _description;
+                cmd.ToExecute += async (client, msg, parameters) =>
+                {
+                    await msg.ReplyAsync(_returnValue);
+                };
+
+                return cmd;
+            }
+        }
+        public List<Command> GetQuickCommands()
+        {
+            List<Command> quickCommands = new List<Command>();
+
+            quickCommands.Add(new QuickCommand("touc", "https://i.imgur.com/3rYGu6V.jpg", "Posts a picture of a toucan").GetCommand());
+
+            return quickCommands;
+        }
+    }
+}
