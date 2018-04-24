95ffaf74499c3369cdaac822f77a1b786b6571ca
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-04-23 17:16:05 -0700

    justask command

--- GenericBot/CommandModules/QuickCommands.cs
+++ GenericBot/CommandModules/QuickCommands.cs
@@ -41,6 +41,7 @@ namespace GenericBot.CommandModules
             List<Command> quickCommands = new List<Command>();
 
             quickCommands.Add(new QuickCommand("touc", "https://i.imgur.com/3rYGu6V.jpg", "Posts a picture of a toucan").GetCommand());
+            quickCommands.Add(new QuickCommand("justask", " If you have a question, don't ask if you can ask it. Just ask it, and someone will be along to help you as soon as they can", "Just ask copypaste").GetCommand());
 
             return quickCommands;
         }
