cedc75e895a258345e32cab1d1513feccfb14f10
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-05-09 06:43:12 -0700

    disconnect alert + moved handlers

--- GenericBot/GenericBot.cs
+++ GenericBot/GenericBot.cs
@@ -97,10 +97,31 @@ namespace GenericBot
                 shard.Ready += OnReady;
                 shard.SetGameAsync(GlobalConfiguration.PlayingStatus).FireAndForget();
 
-                shard.Disconnected += async (exception) => { Disconnects++; };
+                shard.Disconnected += async (exception) =>
+                {
+                    Console.WriteLine($"{exception.Message}\n{exception.StackTrace}");
+                    string dcInfo = $"`{DateTime.Now}`: Disconnected\n";
+                    if (LastCommand.Message.CreatedAt - DateTimeOffset.Now < TimeSpan.FromSeconds(15))
+                    {
+                        dcInfo += $"**(Possibly Related)**\n";
+                    }
+                    var msg = LastCommand.Message as SocketMessage;
+                    dcInfo += $"Last Message: \n" +
+                              $"Time: `{msg.CreatedAt}`\n" +
+                              $"Author: {msg.Author} (`{msg.Author.Id}`)\n" +
+                              $"Guild: {msg.GetGuild().Name} (`{msg.GetGuild().Id}`)\n" +
+                              $"Channel: #{msg.Channel.Name} (`{msg.Channel.Id}`)\n" +
+                              $"Content: `{msg.Content}`\n";
+                    shard.GetApplicationInfoAsync().Result.Owner.SendMessageAsync(dcInfo);
+                    Disconnects++;
+                };
             }
 
             DiscordClient.MessageReceived += MessageEventHandler.MessageRecieved;
+            DiscordClient.JoinedGuild += GuildEventHandler.OnJoinedGuild;
+            DiscordClient.LeftGuild += GuildEventHandler.OnLeftGuild;
+            DiscordClient.UserVoiceStateUpdated += UserEventHandler.UserChangedVc;
+            DiscordClient.UserLeft += UserEventHandler.UserLeft;
 
             var serviceProvider = ConfigureServices();
 
---
+++ GenericBot/GuildEventHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using GenericBot.Entities;
+using Newtonsoft.Json;
+
+namespace GenericBot
+{
+    public static class GuildEventHandler
+    {
+        public static async Task OnJoinedGuild(SocketGuild guild)
+        {
+            if (File.Exists($"files/guildConfigs/{guild.Id}.json"))
+            {
+                GenericBot.GuildConfigs.Add(guild.Id, JsonConvert.DeserializeObject<GuildConfig>(
+                    File.ReadAllText($"files/guildConfigs/{guild.Id}.json")));
+
+                await GenericBot.Logger.LogGenericMessage($"Re-Joined Guild {guild.Id}({guild.Name}) Owned By {guild.Owner.Id}({guild.Owner})");
+
+                string pref = !string.IsNullOrEmpty(GenericBot.GuildConfigs[guild.Id].Prefix)
+                    ? GenericBot.GuildConfigs[guild.Id].Prefix
+                    : GenericBot.GlobalConfiguration.DefaultPrefix;
+                await guild.Owner.GetOrCreateDMChannelAsync().Result.SendMessageAsync($"I'm back on `{guild.Name}`! " +
+                                                                                      $"If you don't remember me, go ahead and do `{pref}config` or `{pref}help`.");
+            }
+            else
+            {
+                await GenericBot.Logger.LogGenericMessage($"Joined Guild {guild.Id}({guild.Name}) Owned By {guild.Owner.Id}({guild.Owner})");
+                await GenericBot.Logger.LogGenericMessage($"Creating GuildConfig for {guild.Id}");
+                string joinMsg =
+                    $"Hey, awesome! Looks like someone (maybe even you) just invited me to your server, `{guild.Name}`! " +
+                    $"If you wanna see everything I can do out of the box, do `{GenericBot.GlobalConfiguration.DefaultPrefix}help`. " +
+                    $"To set me up, do `{GenericBot.GlobalConfiguration.DefaultPrefix}config`";
+                await guild.Owner.GetOrCreateDMChannelAsync().Result.SendMessageAsync(joinMsg);
+                new GuildConfig(guild.Id).Save();
+            }
+        }
+
+        public static async Task OnLeftGuild(SocketGuild guild)
+        {
+            await GenericBot.Logger.LogGenericMessage($"Left {guild.Id}({guild.Name})");
+        }
+
+    }
+}
