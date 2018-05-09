72621cf6fe1d735d2dc7a45d6ba861ecdc60778f
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-05-09 06:48:43 -0700

    Added stuff to config

--- GenericBot/CommandModules/ConfigCommands.cs
+++ GenericBot/CommandModules/ConfigCommands.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Discord.WebSocket;
 using GenericBot.Entities;
 using Newtonsoft.Json;
 
@@ -15,7 +17,7 @@ namespace GenericBot.CommandModules
             List<Command> ConfigCommands = new List<Command>();
 
             Command config = new Command("config");
-            config.Usage = "config <option> <value>`\nOptions are: `adminroles`, `moderatorroles`, `userroles`, `twitter`, `prefix";
+            config.Usage = "config <option> <value>`\nOptions are: `adminroles`, `moderatorroles`, `userroles`, `twitter`, `voicerole, `user`, `mutedroleid";
             config.Description = "Configure the bot's option";
             config.RequiredPermission = Command.PermissionLevels.Admin;
             config.ToExecute += async (client, msg, paramList) =>
@@ -225,6 +227,143 @@ namespace GenericBot.CommandModules
                 }
 
                 #endregion Prefix
+
+                #region UserEvents
+
+                else if (paramList[0].ToLower().Equals("user"))
+                {
+                    if (paramList[1].ToLower().Equals("channelid"))
+                    {
+                        if (paramList.Count() == 2)
+                        {
+                            await msg.ReplyAsync(
+                                $"Current user event channel: <#{GenericBot.GuildConfigs[msg.GetGuild().Id].UserLogChannelId}>");
+                        }
+                        else
+                        {
+                            ulong cId;
+                            if (ulong.TryParse(paramList[2], out cId) && (msg.GetGuild().Channels.Any(c => c.Id == cId) || cId == 0))
+                            {
+                                GenericBot.GuildConfigs[msg.GetGuild().Id].UserLogChannelId = cId;
+                                await msg.ReplyAsync(
+                                    $"User event channel set to <#{GenericBot.GuildConfigs[msg.GetGuild().Id].UserLogChannelId}>");
+                            }
+                            else await msg.ReplyAsync("Invalid Channel Id");
+                        }
+                    }
+                    else if (paramList[1].ToLower().Equals("joinmessage"))
+                    {
+                        if (paramList.Count() == 2)
+                        {
+                            await msg.ReplyAsync(
+                                $"Current user joined message: `{GenericBot.GuildConfigs[msg.GetGuild().Id].UserJoinedMessage.Replace("`", "'")}`");
+                        }
+                        else
+                        {
+                            paramList.RemoveRange(0, 2);
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].UserJoinedMessage = paramList.reJoin();
+                            await msg.ReplyAsync($"User Join message set to `{GenericBot.GuildConfigs[msg.GetGuild().Id].UserJoinedMessage.Replace("`", "'")}`");
+                        }
+                    }
+                    else if (paramList[1].ToLower().Equals("leavemessage"))
+                    {
+                        if (paramList.Count() == 2)
+                        {
+                            await msg.ReplyAsync(
+                                $"Current user left message: `{GenericBot.GuildConfigs[msg.GetGuild().Id].UserLeftMessage.Replace("`", "'")}`");
+                        }
+                        else
+                        {
+                            paramList.RemoveRange(0, 2);
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].UserLeftMessage = paramList.reJoin();
+                            await msg.ReplyAsync($"User Join message set to `{GenericBot.GuildConfigs[msg.GetGuild().Id].UserLeftMessage.Replace("`", "'")}`");
+                        }
+                    }
+                    else if (paramList[1].ToLower().Equals("info"))
+                    {
+                        if (paramList[2].ToLower().Equals(("true")))
+                        {
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].UserJoinedShowModNotes = true;
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].UserLogTimestamp= true;
+                        }
+                        else if (paramList[2].ToLower().Equals(("false")))
+                        {
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].UserJoinedShowModNotes = false;
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].UserLogTimestamp= false;
+                        }
+                        else
+                        {
+                            await msg.ReplyAsync("Invalid Option");
+                        }
+                    }
+                }
+
+                #endregion UserEvents
+
+                #region VoiceChannelRoles
+
+                else if (paramList[0].ToLower().Equals("voicerole"))
+                {
+                    paramList.RemoveAt(0);
+                    if (paramList.Count != 2)
+                    {
+                        await msg.ReplyAsync("Incorrect number of arguments. Make sure the command is `voicerole [VoiceChannelId] [RoleId]`");
+                        return;
+                    }
+                    ulong channelId, roleId;
+                    if (ulong.TryParse(paramList[0], out channelId) && ulong.TryParse(paramList[1], out roleId))
+                    {
+                        SocketVoiceChannel vc;
+                        SocketRole role;
+                        if (msg.GetGuild().VoiceChannels.HasElement(c => c.Id == channelId, out vc) &&
+                            msg.GetGuild().Roles.HasElement(r => r.Id == roleId, out role))
+                        {
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].VoiceChannelRoles.Override(channelId, roleId);
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].Save();
+                            await msg.ReplyAsync($"Anyone who joins the {vc.Name} voice channel will get the {role.Name} role!");
+                        }
+                        else
+                        {
+                            await msg.ReplyAsync("One of those IDs does not exist on this server");
+                        }
+                    }
+                    else
+                    {
+                        await msg.ReplyAsync(("Role or Channel ID is in an incorrect format"));
+                    }
+
+                }
+
+                #endregion VoiceChannelRoles
+
+                #region MutedRoleId
+
+                else if (paramList[0].ToLower().Equals("mutedroleid"))
+                {
+                    paramList.RemoveAt(0);
+                    if (paramList.Count != 1)
+                    {
+                        await msg.ReplyAsync(
+                            "Incorrect number of arguments. Make sure the command is `voicerole [VoiceChannelId] [RoleId]`");
+                        return;
+                    }
+                    else
+                    {
+                        ulong roleId;
+                        if (ulong.TryParse(paramList[0], out roleId) && (msg.GetGuild().Roles.Any(r => r.Id == roleId) || roleId == 0))
+                        {
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].MutedRoleId = roleId;
+                            GenericBot.GuildConfigs[msg.GetGuild().Id].Save();
+                            await msg.ReplyAsync($"MutedRoleId is now `{roleId}`");
+                        }
+                        else
+                        {
+                            await msg.ReplyAsync("Invalid Role Id");
+                        }
+                    }
+                }
+
+                #endregion MutedRoleId
 
                 else await msg.ReplyAsync($"Unknown property `{paramList[0]}`.");
 
