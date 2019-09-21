3ad994506ab8793068515e043c4710eda6946848
MasterChief-John-117 <github@mastrchef.rocks>
2019-09-21 03:23:35 -0400

    Add Logger

--- GenericBot/GenericBot.cs
+++ GenericBot/GenericBot.cs
@@ -1,4 +1,5 @@
-﻿using GenericBot.Entities;
+﻿using Discord.WebSocket;
+using GenericBot.Entities;
 using System;
 
 namespace GenericBot
@@ -6,8 +7,11 @@ namespace GenericBot
     class GenericBot
     {
         private static GlobalConfiguration globalConfig;
+        private static DiscordShardedClient discordClient;
+        public static Logger Logger;
         static void Main(string[] args)
         {
+            Logger = new Logger();
             globalConfig = new GlobalConfiguration().Load();
 
         }
