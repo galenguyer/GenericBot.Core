1ef0d0df3c8d95bf7afa18fa83a4188044fac781
MasterChief-John-117 <github@mastrchef.rocks>
2019-09-21 02:44:24 -0400

    Intialize project

---
+++ GenericBot/GenericBot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericBot
+{
+    class GenericBot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
