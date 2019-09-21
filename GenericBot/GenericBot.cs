1a3e8f6adf5a6d694e9cf3091a1e1785c4820d63
MasterChief-John-117 <github@mastrchef.rocks>
2019-09-21 03:25:17 -0400

    Fetch BuildId if exists

--- GenericBot/GenericBot.cs
+++ GenericBot/GenericBot.cs
@@ -1,6 +1,7 @@
 ﻿using Discord.WebSocket;
 using GenericBot.Entities;
 using System;
+using System.IO;
 
 namespace GenericBot
 {
@@ -8,12 +9,19 @@ namespace GenericBot
     {
         private static GlobalConfiguration globalConfig;
         private static DiscordShardedClient discordClient;
+
         public static Logger Logger;
+        public static string BuildId;
         static void Main(string[] args)
         {
             Logger = new Logger();
             globalConfig = new GlobalConfiguration().Load();
 
+            if (File.Exists("version.txt"))
+            {
+                Logger.LogGenericMessage($"Build {File.ReadAllText("version.txt").Trim()}");
+                BuildId = File.ReadAllText("version.txt").Trim();
+            }
         }
     }
 }
