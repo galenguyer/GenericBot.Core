76084ea6e187910fec243ae1d4f78d9ccd741c1f
MasterChief-John-117 <github@mastrchef.rocks>
2019-09-23 22:34:15 -0400

    Add aliases

---
+++ GenericBot/Entities/Alias.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericBot.Entities
+{
+    public class CustomAlias
+    {
+        public string Alias;
+        public string Command;
+
+        public CustomAlias(string c, string a)
+        {
+            this.Command = c;
+            this.Alias = a;
+        }
+    }
+}
