9b2632f3c6dc4a6fc8f544d3c345eb2362f78ecb
MasterChief-John-117 <github@mastrchef.rocks>
2019-09-24 09:09:32 -0400

    Added quickcommands
    Includes github and justask, ref #73

---
+++ GenericBot/CommandModules/QuickCommands.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using GenericBot.Entities;
+
+namespace GenericBot.CommandModules
+{
+    public class QuickCommands
+    {
+        protected class QuickCommand
+        {
+            private string _name;
+            private string _returnValue;
+            private string _description;
+            public QuickCommand(string name, string returnValue, string description = "")
+            {
+                this._name = name;
+                this._returnValue = returnValue;
+                this._description = string.IsNullOrEmpty(description) ? null : description;
+            }
+
+            public Command GetCommand()
+            {
+                Command cmd = new Command(_name);
+                cmd.SendTyping = false;
+                cmd.Description = _description;
+                cmd.ToExecute += async (context) =>
+                {
+                    await context.Message.ReplyAsync(_returnValue);
+                };
+
+                return cmd;
+            }
+        }
+        public List<Command> GetQuickCommands()
+        {
+            List<Command> quickCommands = new List<Command>();
+
+            quickCommands.Add(new QuickCommand("justask", " If you have a question, don't ask if you can ask it. Just ask it, and someone will be along to help you as soon as they can", "Just ask copypaste").GetCommand());
+            quickCommands.Add(new QuickCommand("github", "https://github.com/MasterChief-John-117/GenericBot", "Link the bot's github repo").GetCommand());
+
+            return quickCommands;
+        }
+    }
+}
