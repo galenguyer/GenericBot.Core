3afde6578ec3142eaab564b05a42250e2e1eec99
Ske <voltasalt@gmail.com>
2019-10-22 22:37:25 +0200

    Update names of context variable; remove SetTyping call

--- GenericBot/CommandModules/GetGuildModule.cs
+++ GenericBot/CommandModules/GetGuildModule.cs
@@ -16,45 +16,44 @@ namespace GenericBot.CommandModules
             
             Command getGuildCommand = new Command("getguild");
             getGuildCommand.Usage = "getguild <user|guild|name>";
-            getGuildCommand.SendTyping = true;
             getGuildCommand.Description = "Gets information about guilds this bot is in.";
             getGuildCommand.RequiredPermission = Command.PermissionLevels.GlobalAdmin;
-            getGuildCommand.ToExecute = async (ctx) =>
+            getGuildCommand.ToExecute = async (context) =>
             {
-                if (MentionUtils.TryParseUser(ctx.ParameterString, out var userId))
+                if (MentionUtils.TryParseUser(context.ParameterString, out var userId))
                 {
                     if (Core.DiscordClient.GetUser(userId) is SocketUser user)
-                        await PrintUserGuilds(ctx, user);
-                    else await ctx.Message.ReplyAsync($"User with ID `{userId}` not found.");
+                        await PrintUserGuilds(context, user);
+                    else await context.Message.ReplyAsync($"User with ID `{userId}` not found.");
                 }
-                else if (ulong.TryParse(ctx.ParameterString, out var id))
+                else if (ulong.TryParse(context.ParameterString, out var id))
                 {
                     if (Core.DiscordClient.GetGuild(id) is SocketGuild guild)
-                        await PrintGuildInformation(ctx, guild);
+                        await PrintGuildInformation(context, guild);
                     else if (Core.DiscordClient.GetUser(id) is SocketUser user)
-                        await PrintUserGuilds(ctx, user);
-                    else await ctx.Message.ReplyAsync($"User or guild with ID `{userId}` not found.");
+                        await PrintUserGuilds(context, user);
+                    else await context.Message.ReplyAsync($"User or guild with ID `{userId}` not found.");
                 }
-                else await PerformGuildSearch(ctx, ctx.ParameterString);
+                else await PerformGuildSearch(context, context.ParameterString);
             };
             commands.Add(getGuildCommand);
 
             return commands;
         }
 
-        private async Task PerformGuildSearch(ParsedCommand ctx, string searchQuery)
+        private async Task PerformGuildSearch(ParsedCommand context, string searchQuery)
         {
             var foundGuilds = Core.DiscordClient.Guilds.Where(g => g.Name.Contains(searchQuery, StringComparison.InvariantCultureIgnoreCase)).ToList();
 
             if (foundGuilds.Count == 0)
             {
-                await ctx.Message.ReplyAsync($"Search query `{searchQuery}` did not return any resulting guilds.");
+                await context.Message.ReplyAsync($"Search query `{searchQuery}` did not return any resulting guilds.");
                 return;
             }
             
             if (foundGuilds.Count > 25)
             {
-                await ctx.Message.ReplyAsync(
+                await context.Message.ReplyAsync(
                     $"Search query `{searchQuery} resulted in **{foundGuilds.Count}** guilds. Try constraining your search term.");
                 return;
             }
@@ -63,10 +62,10 @@ namespace GenericBot.CommandModules
             foreach (var foundGuild in foundGuilds)
                 eb.AddField(foundGuild.Name, $"**ID:** {foundGuild.Id}\n**Members:** {foundGuild.MemberCount}\n**Owned by:** {foundGuild.Owner.Username}#{foundGuild.Owner.Discriminator} ({foundGuild.OwnerId})");
 
-            await ctx.Channel.SendMessageAsync(embed: eb.Build());
+            await context.Channel.SendMessageAsync(embed: eb.Build());
         }
 
-        private async Task PrintGuildInformation(ParsedCommand ctx, SocketGuild guild)
+        private async Task PrintGuildInformation(ParsedCommand context, SocketGuild guild)
         {
             var eb = new EmbedBuilder()
                 .WithTitle($"{guild.Name}")
@@ -74,22 +73,22 @@ namespace GenericBot.CommandModules
                 .AddField("Members", guild.MemberCount)
                 .AddField("Created on", $"{guild.CreatedAt:yyyy-MM-dd HH\\:mm\\:ss zzzz} UTC")
                 .AddField("Owner", $"{guild.Owner.Username}#{guild.Owner.Discriminator} ({guild.OwnerId})");
-            await ctx.Channel.SendMessageAsync(embed: eb.Build());
+            await context.Channel.SendMessageAsync(embed: eb.Build());
         }
 
-        private async Task PrintUserGuilds(ParsedCommand ctx, SocketUser user)
+        private async Task PrintUserGuilds(ParsedCommand context, SocketUser user)
         {
             var eb = new EmbedBuilder().WithTitle($"Guilds owned by user {user.Username}#{user.Discriminator} ({user.Id})");
             var ownedGuilds = Core.DiscordClient.Guilds.Where(g => g.OwnerId == user.Id).Take(25).ToList();
             if (ownedGuilds.Count == 0)
             {
-                await ctx.Message.ReplyAsync($"User {user.Username}#{user.Discriminator} ({user.Id}) does not own any guilds known by this bot.");
+                await context.Message.ReplyAsync($"User {user.Username}#{user.Discriminator} ({user.Id}) does not own any guilds known by this bot.");
             }
             else
             {
                 foreach (var ownedGuild in ownedGuilds)
                     eb.AddField(ownedGuild.Name, $"**ID:** {ownedGuild.Id}\n**Members:** {ownedGuild.MemberCount}");
-                await ctx.Channel.SendMessageAsync(embed: eb.Build());
+                await context.Channel.SendMessageAsync(embed: eb.Build());
             }
         }
     }
