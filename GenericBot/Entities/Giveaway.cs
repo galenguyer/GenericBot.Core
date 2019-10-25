58771139135090a00f07f32e4b01da94cfc15e82
MasterChief-John-117 <github@mastrchef.rocks>
2019-10-25 10:52:08 -0400

    Add Giveaway object

---
+++ GenericBot/Entities/Giveaway.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBot.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class Giveaway
+    {
+        public ulong OwnerId { get; set; }
+        public string Description { get; set; }
+        public string GiveawayId { get; set; }
+        public List<ulong> EnteredUsers { get; set; }
+        public bool IsActive { get; set; }
+
+        public Giveaway()
+        {
+
+        }
+    }
+}
