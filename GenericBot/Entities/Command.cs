5d433ffce7c29ca00773368fb4797a60a24ecd6f
Galen G <github@mastrchef.rocks>
2019-11-20 10:32:52 -0500

    I'm sorry, Dave

--- GenericBot/Entities/Command.cs
+++ GenericBot/Entities/Command.cs
@@ -43,7 +43,10 @@ namespace GenericBot.Entities
         public async Task ExecuteCommand(ParsedCommand command)
         {
             if (GetPermissions(command) < RequiredPermission)
+            {
+                await command.Message.ReplyAsync($"I'm sorry, {command.Author.GetDisplayName()}. I'm afraid I can't do that.");
                 return;
+            }
 
             if (this.RequiredPermission >= PermissionLevels.Moderator && this.RequiredPermission < PermissionLevels.GlobalAdmin && this.Name != "audit")
                 Core.AddToAuditLog(command, command.Guild.Id);
