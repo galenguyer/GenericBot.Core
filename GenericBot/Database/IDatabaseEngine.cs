c9db2305bc0f97e746f9b3db58f908f78982ca3c
MasterChief-John-117 <github@mastrchef.rocks>
2020-03-06 20:24:15 -0500

    Beautifully comment IDatabaseEngine

--- GenericBot/Database/IDatabaseEngine.cs
+++ GenericBot/Database/IDatabaseEngine.cs
@@ -6,24 +6,33 @@ using System.Threading.Tasks;
 
 namespace GenericBot.Database
 {
+    /// <summary>
+    /// Describes all the methods the database engine should provide. The implementation of the methods is
+    /// up to each individual engine. Failure to provide a valid (but not necesarrily correct) object may 
+    /// result in unintended behavior or lack of functionality. It is up to the author of the engine to implement
+    /// all methods correctly and consistently. All database access should be through the static Core class
+    /// to allow for caching and any other sanity checks.
+    /// </summary>
     interface IDatabaseEngine
     {
         /// <summary>
-        /// Get the guild config if it's in the database, return a new config if not
+        /// Return the <see cref="GuildConfig"/> for a guild as specified by the <paramref name="guildId"/>, 
+        /// or return a new <see cref="GuildConfig"/> if none is found
         /// </summary>
-        /// <param name="guildId">The GuildId to look up</param>
-        /// <returns>A populated GuildConfig if found, otherwise a blank GuildConfig</returns>
+        /// <param name="guildId"></param>
+        /// <returns></returns>
         public GuildConfig GetGuildConfig(ulong guildId);
 
         /// <summary>
-        /// Update the config in the database if it exists, add it if not
+        /// Write <paramref name="guildConfig"/> to a guild's database and return it. The database to write to should be determined by the Id field on the <see cref="GuildConfig"/>
         /// </summary>
-        /// <param name="guildConfig">The GuildConfig to save</param>
-        /// <returns>The saved GuildConfig</returns>
+        /// <param name="guildConfig"></param>
+        /// <returns></returns>
         public GuildConfig SaveGuildConfig(GuildConfig guildConfig);
 
+
         /// <summary>
-        /// Add a command to the database, overwrite if exists already.
+        /// Add or update a <see cref="CustomCommand"/> to a guild's database, using the Name field to deduplicate
         /// </summary>
         /// <param name="command"></param>
         /// <param name="guildId"></param>
@@ -31,44 +40,46 @@ namespace GenericBot.Database
         public CustomCommand SaveCustomCommand(CustomCommand command, ulong guildId);
 
         /// <summary>
-        /// Retrieve all Custom Commands from the database
+        /// Return all <see cref="CustomCommand"/>s from a guild's database
         /// </summary>
         /// <param name="guildId"></param>
         /// <returns></returns>
         public List<CustomCommand> GetCustomCommands(ulong guildId);
 
         /// <summary>
-        /// Delete a Custom Command from the database
+        /// Delete all <see cref="CustomCommand"/>s matching a <paramref name="name"/> from a guild's database
         /// </summary>
         /// <param name="name"></param>
         /// <param name="guildId"></param>
         public void DeleteCustomCommand(string name, ulong guildId);
 
         /// <summary>
-        /// Look for a user in the guild's database, return a new user if not found
+        /// Retrieve a <see cref="DatabaseUser"/> by <paramref name="userId"/> and <paramref name="guildId"/>
         /// </summary>
         /// <param name="userId"></param>
         /// <param name="guildId"></param>
+        /// <param name="log">Whether or not to log the request</param>
         /// <returns></returns>
         public DatabaseUser GetUserFromGuild(ulong userId, ulong guildId, bool log = true);
 
         /// <summary>
-        /// Insert a user into a guild's database
+        /// Add or update a <see cref="DatabaseUser"/> to a guild's database, using the Id field to deduplicate
         /// </summary>
         /// <param name="user"></param>
         /// <param name="guildId"></param>
+        /// <param name="log">Whether or not to log the request</param>
         /// <returns></returns>
         public DatabaseUser SaveUserToGuild(DatabaseUser user, ulong guildId, bool log = true);
 
         /// <summary>
-        /// Retrieve all users from a guild's database, useful for finding from nicknames
+        /// Retreive all <see cref="DatabaseUsers"/>s from a guild's database
         /// </summary>
         /// <param name="guildId"></param>
         /// <returns></returns>
         public List<DatabaseUser> GetAllUsers(ulong guildId);
 
         /// <summary>
-        /// Save a GenericBan to a guild
+        /// Add or update <see cref="GenericBan"/> to a guild's database, using the Id field for deduplication
         /// </summary>
         /// <param name="ban"></param>
         /// <param name="guildId"></param>
@@ -76,21 +87,22 @@ namespace GenericBot.Database
         public GenericBan SaveBanToGuild(GenericBan ban, ulong guildId);
 
         /// <summary>
-        /// Get all bans saved to a guild
+        /// Retrieve all <see cref="GenericBan"/>s from a guild's database
         /// </summary>
         /// <param name="guildId"></param>
+        /// <param name="log">Whether or not to log the request</param>
         /// <returns></returns>
         public List<GenericBan> GetBansFromGuild(ulong guildId, bool log = true);
 
         /// <summary>
-        /// Remove a ban from a guild after it's expired
+        /// Delete a <see cref="GenericBan"/> from a guild's database 
         /// </summary>
-        /// <param name="banId"></param>
+        /// <param name="userId"></param>
         /// <param name="guildId"></param>
-        public void RemoveBanFromGuild(ulong banId, ulong guildId);
+        public void RemoveBanFromGuild(ulong userId, ulong guildId);
 
         /// <summary>
-        /// Create a new Quote object and save it to the database
+        /// Add a <see cref="Quote"/> to a guild's database, without any deduplication
         /// </summary>
         /// <param name="quote"></param>
         /// <param name="guildId"></param>
@@ -98,7 +110,7 @@ namespace GenericBot.Database
         public Quote AddQuote(string quote, ulong guildId);
 
         /// <summary>
-        /// Mark a quote as inactive in the database
+        /// Update a <see cref="Quote"/>'s Active field to be inactive, using the Quote's Id field for identification
         /// </summary>
         /// <param name="id"></param>
         /// <param name="guildId"></param>
@@ -106,45 +118,78 @@ namespace GenericBot.Database
         public bool RemoveQuote(int id, ulong guildId);
 
         /// <summary>
-        /// Get all quotes from a guild to search over
+        /// Retrieve all <see cref="Quote"/>s from a guild's database
         /// </summary>
         /// <param name="guildId"></param>
         /// <returns></returns>
         public List<Quote> GetAllQuotes(ulong guildId);
 
         /// <summary>
-        /// Add an event to a guild's audit log
+        /// Add a <see cref="ParsedCommand"/> to a guild's database, without any deduplication
         /// </summary>
         /// <param name="command"></param>
         /// <param name="guildId"></param>
         public void AddToAuditLog(ParsedCommand command, ulong guildId);
 
         /// <summary>
-        /// Get all entries from a guild's audit log
+        /// Retrieve all <see cref="AuditCommand"/>s from a guild's database
         /// </summary>
         /// <param name="guildId"></param>
         /// <returns></returns>
         public List<AuditCommand> GetAuditLog(ulong guildId);
 
+        // TODO: Make this take a VerificationEvent instead of a UserId
         /// <summary>
-        /// Save a verification to a guild's database
+        /// Add a <see cref="VerificationEvent"/> to a guild's database, without any deduplication
         /// </summary>
         /// <param name="userId"></param>
         /// <param name="guildId"></param>
         public void AddVerification(ulong userId, ulong guildId);
 
+        // TODO: Check responsibilities for the next two methods, as well as simplify them
+        /// <summary>
+        /// Add a <see cref="Giveaway"/> to a guild's database, using the Id field to deduplicate
+        /// </summary>
+        /// <param name="giveaway"></param>
+        /// <param name="guildId"></param>
+        /// <returns></returns>
         public Giveaway CreateGiveaway(Giveaway giveaway, ulong guildId);
 
+        /// <summary>
+        /// Add or update a <see cref="Giveaway"/> to a guild's database, using the Id field to deduplicate
+        /// </summary>
+        /// <param name="giveaway"></param>
+        /// <param name="guildId"></param>
+        /// <returns></returns>
         public Giveaway UpdateOrCreateGiveaway(Giveaway giveaway, ulong guildId);
 
+        /// <summary>
+        /// Retrieve all <see cref="Giveaway"/>s from a guild's database
+        /// </summary>
+        /// <param name="guildId"></param>
+        /// <returns></returns>
         public List<Giveaway> GetGiveaways(ulong guildId);
 
+        /// <summary>
+        /// Remove a <see cref="Giveaway"/> from a guild's database using the <paramref name="giveaway"/>'s Id field
+        /// </summary>
+        /// <param name="giveaway"></param>
+        /// <param name="guildId"></param>
         public void DeleteGiveaway(Giveaway giveaway, ulong guildId);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="status"></param>
         public void AddStatus(Status status);
 
+        /// <summary>
+        /// Add or update an <see cref="ExceptionReport"/> to the global database, using the Id field to deduplicate. 
+        /// If a matching <see cref="ExceptionReport"/> exists, increment the Seen counter and return the <see cref="ExceptionReport"/>
+        /// from the database
+        /// </summary>
+        /// <param name="report"></param>
+        /// <returns>The reported exception with any updated metadata</returns>
         public ExceptionReport AddOrUpdateExceptionReport(ExceptionReport report);
-
-        public List<string> GetGuildIdsFromDb();
     }
 }
