fdab10ca2052373c898c1b8dc2c7e5c6076fa2fe
Galen Guyer <git@galenguyer.com>
2020-04-10 19:04:07 -0700

    Add userinfo endpoint

---
+++ GenericBot/Controllers/UserInfoController.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using GenericBot.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace GenericBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInfoController : ControllerBase
+    {
+        // GET: api/userinfo
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                WebClient client = new WebClient();
+
+                client.Headers.Add("Authorization", $"Bearer {Request.Cookies["Authorization"]}");
+                string userData = client.DownloadString("https://discordapp.com/api/v6/users/@me");
+
+                PartialUser pUser = JsonConvert.DeserializeObject<PartialUser>(userData);
+
+                return new OkObjectResult(pUser);
+            }
+            catch
+            {
+                return new BadRequestResult();
+            }
+        }
+    }
+}
