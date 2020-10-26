75b3dc244dee074285555908d0781fd4ba1cad8b
Galen Guyer <galen@galenguyer.com>
2020-10-26 00:13:37 -0400

    Add get all quotes route

---
+++ GenericBot/Controllers/QuotesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenericBot.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        // GET api/v1/guilds/[GuildId]/quotes
+        // GET all quotes for a guild
+        [HttpGet("/api/v1/guild/{guildId}/quotes")]
+        public IActionResult GetAllQuotes(ulong guildId)
+        {
+            return new JsonResult(Core.GetAllQuotes(guildId));
+        }
+    }
+}
