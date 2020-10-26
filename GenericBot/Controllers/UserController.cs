2935c418d9c598551ed56d350871a5ed2032d442
Galen Guyer <galen@galenguyer.com>
2020-10-26 11:06:19 -0400

    add user route

---
+++ GenericBot/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericBot.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/v1/user/UserId
+        [Authorize]
+        [HttpGet("/api/v1/user")]
+        public IActionResult Get()
+        {
+            ulong userId = ulong.Parse(User.Claims.First(claim => claim.Type == ClaimTypes.NameIdentifier).Value);
+            var user = Core.DiscordClient.GetUser(userId);
+
+            var userData = new { 
+                username = user.Username, 
+                discriminator = user.Discriminator 
+            };
+
+            return new JsonResult(userData);
+        }
+    }
+}
